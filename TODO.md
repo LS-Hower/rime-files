4b3bc55a8d93bdb54289dc6fdae9a967e56794f9
LS_Hower <ls_hower@qq.com>
2025-12-28 18:40:01 +0800

    Refactored fetcher script
    - Use `pathlib.Path` instead of `os.path`.
    - Use `logging` instead of `print`.
    - Written in a more functional style.

--- TODO.md
+++ TODO.md
@@ -1,6 +1,3 @@
 - 尝试五笔单字流
 - 为 `touhou` 词库添加新作内容
 - 逐一比对，将 `hower_private` 词库内容集齐： `hower_private` = 旧 `touhou` + 旧 `hower` - 新 `touhou` - 新 `hower`
-- 重构 `fetch_from_local.py` ：
-    - 使用 `pathlib` 模块处理路径，代替 `os.path`
-    - 抽出函数式部分
--- src/fetch_from_local.py
+++ src/fetch_from_local.py
@@ -1,89 +1,137 @@
 from __future__ import annotations
-from typing import TypedDict, NotRequired, Optional
-from collections.abc import Callable
+from collections.abc import Callable, Generator
+from dataclasses import dataclass
 from itertools import takewhile
-import os
+from pathlib import Path
+from typing import NotRequired, Optional, TypedDict
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # 允许自定义文件获取方式。
-type FileFetcher = Callable[[str], str]
-type FileNameModifier = Callable[[str], str]
-type FileDescription = str | tuple[str, FileFetcher, FileNameModifier]
+type FileName = str
+type FileContent = str
+type FolderName = str
+type FileFetcher = Callable[[Path], str]
+type FileNameModifier = Callable[[FileName], FileName]
+type FileDescription = FileName | tuple[FileName, FileFetcher, FileNameModifier]
 
 # 用类 JSON 方式描述文件夹结构。
 class FolderDescription(TypedDict):
-    name: str
+    name: FolderName
     files: NotRequired[list[FileDescription]]
     folders: NotRequired[list[FolderDescription]]
 
-def make_prefix_adder(prefix: str) -> FileNameModifier:
-    def adder(filename: str) -> str:
-        return prefix + filename
+def make_prefix_adder(filename_prefix: str) -> FileNameModifier:
+    """
+    纯函数。
+    `make_prefix_adder('partial-')('test.txt')` -> `'partial-test.txt'`
+    """
+    def adder(filename: FileName) -> FileName:
+        return filename_prefix + filename
     return adder
 
-def default_file_fetcher(filename: str) -> str:
-    with open(filename, 'r', encoding='utf-8') as f:
-        return f.read()
-
-def default_filename_modifier(filename: str) -> str:
+def default_filename_modifier(filename: FileName) -> FileName:
+    """纯函数。"""
     return filename
 
-def fetch_wubi86_custom_dict(filename: str) -> str:
-    assert filename.endswith('wubi86.custom.dict.yaml'), "The fetcher designed for wubi86.custom.dict.yaml is not used on it."
-
-    # 读取，直到遇到 '...\n' 行为止。
-    with open(filename, 'r', encoding='utf-8') as f:
-        return ''.join(takewhile(lambda line: line != '...\n', f)) + '...\n'
-
-def fetch_single_file(source_path: str, source_file_name: str,
-                      dest_path: str, dest_file_name: str,
-                      fetcher: FileFetcher = default_file_fetcher) -> None:
-    src = os.path.join(source_path, source_file_name)
-    dest = os.path.join(dest_path, dest_file_name)
-
-    try:
-        content = fetcher(src)
-        # 检查要写入的内容是否过大
-
-        if len(content) > 10 * 1024 * 1024:
-            print(f'[Warning] Content fetched from {src} is large: {len(content)} bytes')
-        with open(dest, 'w', encoding='utf-8') as f:
-            f.write(content)
-    except Exception as e:
-        print(f'[Error] "{e}" occured when fetching {src} -> {dest}')
-
-def get_folder_files(folder: str) -> tuple[str, list[str]]:
-    files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
-    return folder, files
-
-def find_from_sources(filename: str, sources: dict[str, list[str]]) -> Optional[str]:
+def find_from_sources(filename: FileName, sources: dict[Path, set[FileName]]) -> Optional[Path]:
+    """
+    纯函数。
+    在 `sources` 的值中查找 `filename`，返回 `sources` 的键。
+    如果找不到，返回 `None`。
+    """
     for path, files in sources.items():
         if filename in files:
             return path
     return None
 
-def fetch_from_local(structure: FolderDescription, source_paths: list[str]) -> None:
+def default_file_fetcher(file: Path) -> FileContent:
     """
-    从指定的本地路径 `source_paths` 中读取文件，复制到由 `structure` 描述的目录结构中。
+    不是纯函数：需要读取本地文件内容。
+    读取一个文件的内容。
+    """
+    assert file.is_file()
+    return file.read_text(encoding='utf-8')
+
+def fetch_wubi86_custom_dict(file: Path) -> FileContent:
+    """
+    不是纯函数：需要读取本地文件内容。
+    专为 `wubi86.custom.dict.yaml` 设计的文件读取函数。
+    只读取文件头，直到遇到 `'...\n'` 行为止。
+    `'...\n'` 行也会追加到返回的字符串中。
+    """
+    assert file.is_file()
+    assert file.name == 'wubi86.custom.dict.yaml', "The fetcher designed for wubi86.custom.dict.yaml is not used on it."
+
+    with file.open('r', encoding='utf-8') as f:
+        return ''.join(takewhile(lambda line: line != '...\n', f)) + '...\n'
+
+@dataclass
+class FileFetchTask(object):
+    source: Path
+    destination: Path
+    fetcher: FileFetcher = default_file_fetcher
+
+    def __call__(self: FileFetchTask) -> None:
+        """
+        不是纯函数：需要读取和写入本地文件。
+        执行由 `self` 描述的任务：用 `fetcher` 从 `source` 中读取文件内容，写入到 `destination` 中。
+        """
+        logger.info(f'Fetching: {self.source} -> {self.destination}')
+        assert self.source.is_file()
+
+        try:
+            # 如果目标文件夹不存在，创建它
+            if not self.destination.parent.exists():
+                self.destination.parent.mkdir(parents=True, exist_ok=False)
+                logger.info(f'Created directory: {self.destination.parent}')
+            content = self.fetcher(self.source)
+            # 检查要写入的内容是否过大
+            if len(content) > 1 * 1024 * 1024:
+                kb = len(content) / 1024
+                logger.warning(f'Content fetched from {self.source} is large: {kb:.2f} KB')
+            self.destination.write_text(content, encoding='utf-8')
+
+            assert self.destination.is_file()
+
+        except Exception as e:
+            logger.error(f'An error "{e}" occured when fetching {self.source} -> {self.destination}')
+
+
+def iter_fetch_tasks(structure: FolderDescription, source_paths: list[Path]) -> Generator[FileFetchTask]:
+    """
+    不是纯函数：需要读取本地的文件结构。
+
+    依据 `structure`，从指定的本地路径 `source_paths` 中寻找哪些文件可以复制，应该复制到什么地方。
     `structure` 中可能存在自定义的文件读取函数。
     `source_paths` 中靠前的路径优先级更高。
+    返回所有要执行的任务（以生成器形式）。
 
     复制成功 / 失败时打印日志。
     若文件过大，打印日志。
     """
 
     # 获取 source_paths 中所有文件名
+    sources: dict[Path, set[FileName]] = {
+        path: {
+            sub.name
+            for sub in path.iterdir()
+            if sub.is_file()
+        }
+        for path in source_paths
+    }
 
-    sources = dict(get_folder_files(path) for path in source_paths)
+    def iter_folder_recursive(dest_description: FolderDescription, dest_parent_path: Path) -> Generator[FileFetchTask]:
+        """
+        不是纯函数：需要读取本地的文件结构。
+        递归生成对于一个文件夹的任务。
+        """
 
-    def fetch_folder_recursive(dest_description: FolderDescription, dest_parent_path: str) -> None:
-        """递归复制一个文件夹。"""
+        dest_current_path = dest_parent_path / dest_description['name']
 
-        # 先确定要往哪里复制；如果不存在就创建
-        dest_current_path = os.path.normpath(os.path.join(dest_parent_path, dest_description['name']))
-        if not os.path.exists(dest_current_path):
-            os.makedirs(dest_current_path)
-
-        # 复制各个文件
+        # 生成复制各个文件的任务
         if 'files' in dest_description:
             for source_file_description in dest_description['files']:
                 if isinstance(source_file_description, str):
@@ -98,24 +146,25 @@ def fetch_from_local(structure: FolderDescription, source_paths: list[str]) -> N
 
                 # 没能找到
                 if source_path is None:
-                    print(f'[Warning] Could not find {os.path.join(dest_current_path, source_filename)} in sources. Skipped.')
+                    logger.error(f'Could not find {dest_current_path / source_filename} in sources. Skipped.')
                     continue
 
-                # 找到了；复制该文件
+                # 找到了；生成“复制该文件”的任务
                 dest_filename = name_modifier(source_filename)
-                print(f'[Info] Fetching: {source_filename} -> {dest_filename} (paths: {source_path} -> {dest_current_path})')
-                fetch_single_file(
-                    source_path, source_filename,
-                    dest_current_path, dest_filename,
-                    fetcher,
+                task = FileFetchTask(
+                    source = source_path / source_filename,
+                    destination = dest_current_path / dest_filename,
+                    fetcher = fetcher
                 )
 
-        # 复制各个子文件夹
+                yield task
+
+        # 对于子文件夹递归处理
         if 'folders' in dest_description:
             for sub_folder in dest_description['folders']:
-                fetch_folder_recursive(sub_folder, dest_current_path)
+                yield from iter_folder_recursive(sub_folder, dest_current_path)
 
-    fetch_folder_recursive(structure, '.')
+    yield from iter_folder_recursive(structure, Path('.'))
 
 if __name__ == '__main__':
 
@@ -141,13 +190,14 @@ if __name__ == '__main__':
                     'wubi86.zkana.dict.yaml',
                 ],
             },
-        ]
+        ],
     }
 
-    fetch_from_local(
-        rime_structure,
-        [
-            "C:\\Users\\ls_ho\\AppData\\Roaming\\Rime",
-        ],
-    )
+    sources = [
+        Path("C:\\Users\\ls_ho\\AppData\\Roaming\\Rime"),
+    ]
+
+    for task in iter_fetch_tasks(rime_structure, sources):
+        task()
+
 
